eb0bafb633cea036cef0838bcb52ae3010f6d895
Dillon Bordeleau <dillonbordeleau@gmail.com>
2024-10-31 17:20:54 -0400

    made buttons more visible in dark mode

--- components/intro.tsx
+++ components/intro.tsx
@@ -51,16 +51,16 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.1 }}>
-            <Link href="#contact" className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:bg-gray-950 transition-all' onClick={() => {
+            <Link href="#contact" className='group shadow-md bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none hover:bg-gray-950 transition-all dark:border dark:border-white' onClick={() => {
               setActiveSection("Contact");
               setLastClickTime(Date.now);
             }}>Contact me <BsArrowRight className='opacity-70 group-hover:translate-x-2 transition' /></Link>
             
-            <a className='group bg-white hover:bg-gray-100 px-7 py-3 flex items-center gap-2 rounded-full outline-none transition-all cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20 dark:hover:text-white' href='/DillonBordeleauResume.pdf' download={true}>Download my Résumé <HiDownload className='opacity-70 group-hover:translate-y-1 transition'/></a>
+            <a className='group shadow-md bg-white hover:bg-gray-100 px-7 py-3 flex items-center gap-2 rounded-full outline-none transition-all cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:border dark:border-white dark:hover:bg-white/20 dark:hover:text-white' href='/DillonBordeleauResume.pdf' download={true}>Download my Résumé <HiDownload className='opacity-70 group-hover:translate-y-1 transition'/></a>
             
-            <a className='bg-white p-4 flex text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20 dark:hover:text-white' href='https://www.linkedin.com/in/dillonbordeleau/' target='_blank'><BsLinkedin/></a>
+            <a className='bg-white shadow-md p-4 flex text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack dark:border dark:border-white dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20 dark:hover:text-white' href='https://www.linkedin.com/in/dillonbordeleau/' target='_blank'><BsLinkedin/></a>
             
-            <a className='bg-white p-4 flex text-[1.35rem] text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20 dark:hover:text-white' href='https://github.com/DBordeleau/' target='_blank'><FaGithubSquare/></a>
+            <a className='bg-white shadow-md p-4 flex text-[1.35rem] text-gray-700 items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition-all cursor-pointer borderBlack dark:border dark:border-white dark:bg-white/10 dark:text-white/60 dark:hover:bg-white/20 dark:hover:text-white' href='https://github.com/DBordeleau/' target='_blank'><FaGithubSquare/></a>
         </motion.div>
     </section>
   )
